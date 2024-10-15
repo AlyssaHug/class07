aad8be926ed7051c213d0f81491616945e898aa3
alyssa <ahuggins4@my.bcit.ca>
2024-10-15 14:57:05 -0700

    Driving Age

--- 1.js
+++ 1.js
@@ -14,19 +14,27 @@ Create a function that determines the appropriate message for a person based on 
 
 //determine a proper parameter variable name
 function CheckDrivingAge(paramAge){
-
-  console.log("Write your code here!");
+  if(paramAge < 16) {
+    console.log("You're way too young!");
+  } else if(paramAge >= 16 && paramAge <= 17){
+    console.log('You can drive, but make sure you have an adult with you!');
+  } else if(paramAge >= 18 && paramAge <= 70){
+    console.log('You can drive all you want!');
+  } else if(paramAge > 70){
+    console.log('Be safe! Consider getting a driving assessment before going out!')
+  } 
 }
 
-function StartApp(){
-  readline.question('question? ', _variableName => {
 
+function StartApp(){
+  readline.question("I see you're wanting to take this car. Please tell me how old you are.", age => {
+    CheckDrivingAge(Number(age));
     //call your function here.
     
     // readline.close();
-    if(_variableName !== "quit"){
+    if(age !== "exit"){
       StartApp();
-    } else {
+    } else{
       readline.close();
     }
   });
